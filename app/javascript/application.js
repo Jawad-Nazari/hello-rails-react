0f1164666699432da354ff604026139d065a8e67
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-23 18:03:41 +0500

    Add components and make the app functional

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/app";
+import store from "./redux/store";
+
+function Index() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+ReactDOM.render(<Index />, document.getElementById("root"));
