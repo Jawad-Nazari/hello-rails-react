f554e2a11dd8b7d9468132fe4eba433f87944e7a
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-23 18:04:56 +0500

    Add liter configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'root/index'
+    root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+     
+     get 'greetings/random'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Warmest greetings to you!",
+  "Hello, wonderful soul!",
+  "Step into our world with a smile!",
+  "A hearty welcome awaits you!",
+  "Greetings and good vibes incoming!",
+  "Welcome, seeker of greatness!",
+  "Hello, let's paint the future together!"
+]
+
+greetings.each do |text|
+  Message.create(text: text)
+end
